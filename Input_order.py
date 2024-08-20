4c3c572bb6fa69e658d034db150868b9ea8458d0
profession.thamma@gmail.com <profession.thamma@gmail.com>
2024-08-19 20:44:51 -0400

    defining variables

---
+++ Input_order.py
@@ -0,0 +1,19 @@
+# Take input of an item the user wants to purchase
+item = input("what is the iteam you want to purchase?")
+
+# Ask how much the item costs and cast it as a number.
+# What type of number should it be cast as?
+cost = int(input("what is cost of the item?"))
+
+# Ask what quantity of the item should be purchased and cast it as a number.
+# What type of number should it be cast as?
+quantity = int(input("How many do you want to buy?"))
+
+# Print the item cost along with its data type
+print("Cost of each item is", cost, "data type of cost",type(cost))
+
+# Print the item quantity along with its data type
+print("how many items are we purchasing", quantity, "data type of quantity is ", type(quantity))
+
+# Print results
+print("Item I want to buy is ", item, "How many I want to buy", quantity, "cost of each item", cost)
--- laudry_Pseudocode.md
+++ laudry_Pseudocode.md
@@ -1,8 +1,8 @@
 # Algorithm: Sorting Laundry
 
 # Tasks:
- create four baskets for each type of clothes (white, red, delicate and everything else)
- 
+#create four baskets for each type of clothes (white, red, delicate and everything else)
+
 # Pattern recognition:
 
 # Abstraction:
