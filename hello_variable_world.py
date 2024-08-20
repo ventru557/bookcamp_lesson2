2629a2a51058c22c468cb9bd57773a38ca4a9235
profession.thamma@gmail.com <profession.thamma@gmail.com>
2024-08-19 20:12:18 -0400

    added python code

---
+++ hello_variable_world.py
@@ -0,0 +1,22 @@
+# Create a variable called 'full_name' that holds a string
+full_name = "Venlat Varun Thamma"
+# Create a variable called 'country_to_visit' that holds a string
+country_to_visit = 'India'
+# Create a variable called 'days' that holds an integer of the number of days
+# for the vacation
+days = 10
+# Create a variable called 'exchange_rate' that holds a float of the currency
+# exchange rate for the country to visit
+exchange_rate = 83.95
+# Create a variable called 'satisfied' that holds a Boolean
+satisfied = True
+# Print out "Hello <full_name>"
+print("Hello", full_name)
+# Print out a sentence with the country the user entered
+print("I want to visit", country_to_visit)
+# Print out a sentence with the number of vacation days
+print("Number of vacation days", days)
+# Print out a sentence with the currency exchange rate
+print("what is the currency exchange rate", exchange_rate)
+# Print out a sentence with the value of satisfied
+print("are you satisfied with your vacation?", satisfied)
---
+++ variables.py
@@ -0,0 +1,16 @@
+# Create a variable called 'profession' with a string "software engineer"
+profession = "software engineer"
+# Create a variable called 'years' with an integer 20
+years = 20
+# Create a variable called 'hourly_wage' with a floating point value 72.12
+hourly_wage = 72.12
+# Create a variable called 'expert_status' with the Boolean value of True
+expert_status = True
+# Print a statement with the 'profession' string
+print ("what is your profession", profession)
+# Print a statement with the 'years' integer
+print ("how many years of experience ", years)
+# Print a statement with the 'hourly_wage' float
+print ("what is the hourly wage", hourly_wage)
+# Print a statement with the 'expert_status' Boolean
+print ("Is this individual ann expert", expert_status)
